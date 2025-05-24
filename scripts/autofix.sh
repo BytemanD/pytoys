ee2fbf4c8b6696876693dcc70b32407a99cffd70
BytemanD <byteman.e@outlook.com>
2025-05-25 00:03:42 +0800

    feat: autoflake 格式化移除未使用的模块
    Signed-off-by: BytemanD <byteman.e@outlook.com>

--- scripts/autofix.sh
+++ scripts/autofix.sh
@@ -1,11 +1,15 @@
-echo -e "\033[44;37m     bloack     \033[0m"
+echo -e "\033[44;37m      bloack      \033[0m"
 black src
 echo
 
-echo -e "\033[44;37m     isort      \033[0m"
+echo -e "\033[44;37m    autoflake     \033[0m"
+autoflake -ri  --remove-all-unused-imports  src
+echo
+
+echo -e "\033[44;37m      isort       \033[0m"
 isort src
 echo
 
-echo -e "\033[44;37m     pylint     \033[0m"
+echo -e "\033[44;37m      pylint      \033[0m"
 pylint src
 echo
